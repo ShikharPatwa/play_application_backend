86d807f31ff16c4f2ac7952ee63e0a2dca0c4706
ShikharPatwa <shikharpatwa786@gmail.com>
2026-09-24 17:59:18 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,7 +11,14 @@ app.use(cors({
 }))
 app.use(express.json({limit:"16kb"}))
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static())
+app.use(express.static("public"))
 app.use(cookieParser())
 
-export default {app}
+// routes import
+import userRouter from "./routes/user.routes.js"
+
+
+// routes declaration
+app.use('/api/v1/users',userRouter)
+
+export default app
